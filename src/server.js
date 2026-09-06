ba9ec32212e20c65552778ef629e83da276059b0
Kittunaik <159645431+Mrkittunaik@users.noreply.github.com>
2026-09-06 21:57:03 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const http = require('http');
 const app = require('./app');
 const connectDB = require('./config/db');
 const sockets = require('./sockets/io');
+const cron = require('./utils/cron');
 
 const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   const server = http.createServer(app);
   sockets.init(server);
+  cron.start();
   server.listen(PORT, () => console.log(`[server] milk-backend (HTTP + WebSocket) running on port ${PORT}`));
 });
